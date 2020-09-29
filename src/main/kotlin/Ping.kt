1a225ea5db40dde9f54b5c2b655b75bb66b3851f
haxul <s.s.starodubov@yandex.ru>
2020-09-29 21:24:36 +0400

    init

--- src/main/kotlin/Ping.kt
+++ src/main/kotlin/Ping.kt
@@ -1,6 +1,10 @@
 import org.apache.solr.client.solrj.impl.HttpSolrClient
 import org.apache.solr.client.solrj.impl.XMLResponseParser
 import org.apache.solr.common.SolrInputDocument
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 object SolrClient {
     fun get(): HttpSolrClient {
@@ -17,9 +21,16 @@ fun main() {
     val solr = SolrClient.get()
 
     val document = SolrInputDocument()
-    document.addField("id", "123456")
-    document.addField("name", "Kenmore Dishwasher")
-    document.addField("price", "599.99")
+    document.addField("id", "16")
+    document.addField("name", "Kenmore")
+    document.addField("price", "23599.99")
     solr.add(document)
     solr.commit()
+    
+    val client = HttpClient.newBuilder().build();
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("http://localhost:8983/solr/test_core/select?q=*%3A*"))
+        .build();
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    print(response.body())
 }
